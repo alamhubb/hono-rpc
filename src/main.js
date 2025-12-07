b21707d50bac2875d644fa23b58c8cb7f1b14d64
alamhubb <alamhubb@163.com>
2025-12-07 13:50:02 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { signal } from 'alien-signals'
+import { div, button, render } from './solid-runtime.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// 创建响应式状态
+const count = signal(0)
 
-setupCounter(document.querySelector('#counter'))
+// 使用 SolidJS 风格的运行时语法构建 UI
+const app = div({
+  children: [
+    div({
+      class: 'card',
+      children: [
+        button({
+          type: 'button',
+          // 响应式文本
+          children: [() => `count is ${count()}`],
+          // 点击事件
+          onclick: () => count(count() + 1)
+        })
+      ]
+    })
+  ]
+})
+
+// 渲染到 DOM
+render(app, document.querySelector('#app'))
