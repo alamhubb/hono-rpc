fba0d38b886b68657b47a98824c69cd56c73b534
alamhubb <alamhubb@163.com>
2025-12-09 22:39:11 +0800

    更新为纯前端项目

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,7 +4,7 @@ import devServer, { defaultOptions } from '@hono/vite-dev-server';
 export default defineConfig({
     plugins: [
         devServer({
-            entry: 'server.ts', // Hono 应用入口
+            entry: 'server/server.ts', // Hono 应用入口
             // 使用默认排除规则，让 Vite 处理静态资源
             exclude: [...defaultOptions.exclude],
         }),
