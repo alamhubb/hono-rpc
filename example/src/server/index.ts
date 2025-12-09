9ab02f79aafc3756d4d5aa4dbc01a91a9302be85
alamhubb <alamhubb@163.com>
2025-12-10 00:10:42 +0800

    支持静态属性

--- example/src/server/index.ts
+++ example/src/server/index.ts
@@ -1,6 +1,6 @@
 import { useHono } from 'hono-decorator';
-// 导入控制器（装饰器会自动注册）
-import './controllers/HelloController';
+// 测试：删除导入看看会发生什么
+// import './controllers/HelloController';
 
 const app = useHono();
 
